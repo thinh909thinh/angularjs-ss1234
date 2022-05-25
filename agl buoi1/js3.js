c0523daf6e377500be05ce6f17bb828ffd21a486
thinh909thinh <51990581+thinh909thinh@users.noreply.github.com>
2022-05-25 17:07:51 +0700

    Add files via upload

---
+++ agl buoi1/js3.js
@@ -0,0 +1,21 @@
+// var app = angular.module('myApp',[ ]);
+// app.controller('firstControllerScope',function ($scope){
+//     // Initialize the model variable
+//     $scope.firstName = "Chris";
+// });
+// app.controller('secondControllerScope',function ($scope){
+//     $scope.lastName = "Hemsworth";
+//     // Define utility functions
+//     $scope.getFullName = function () {
+//         return $scope.firstName + " "+ $scope.lastName;
+//     };
+// });
+// app.controller('thirdControllerScope',function ($scope){
+//     // Initialize the model variables
+//     $scope.middleName = "Whitelaw";
+//     $scope.lastName = "Pine";
+//     // Define utility functions
+//     $scope.getFullName = function () {
+//         return $scope.firstName + " " + $scope.middleName + " " + $scope.lastName;
+//     };
+// });
